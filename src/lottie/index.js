e04dc999c412cf3c699f4e1982e1b691ecb6c61a
Puzzle9900 <puzzle9900@gmail.com>
2021-03-04 14:28:47 -0500

    2 animations

--- src/lottie/index.js
+++ src/lottie/index.js
@@ -2,11 +2,12 @@ import dayAndNight from "./day-and-night.json";
 import dayNight from "./day-night.json";
 import flowerMograph from "./flower-mograph.json";
 import sunny from "./sunny.json";
-import weatherAppAnimation from "./weather-app-animation.json";
+import airplane from "./airplane.json";
 
 const list = [
   dayAndNight,
   dayNight,
+  airplane,
   flowerMograph,
   sunny,
 ];
